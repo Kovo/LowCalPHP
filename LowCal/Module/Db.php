1e5d6bbc17d3d8c4d8c921e85de0beeb247550bf
kevork <kevork@azzimov.com>
2017-06-13 17:38:11 -0400

    work on modules

---
+++ LowCal/Module/Db.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace LowCal\Module;
+
+/** 
+ * Class Db
+ * @package LowCal\Module
+ */
+class Db extends Module
+{
+
+}
---
+++ LowCal/Module/Log.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace LowCal\Module;
+
+/** 
+ * Class Log
+ * @package LowCal\Module
+ */
+class Log extends Module
+{
+
+}
