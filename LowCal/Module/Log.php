d0d35ac2d7c183c0bf717e219efbb6c6620ddfac
kevork <kevork@azzimov.com>
2017-06-14 17:03:52 -0400

    work on modules

--- LowCal/Module/Log.php
+++ LowCal/Module/Log.php
@@ -14,16 +14,74 @@ class Log extends Module
 	 */
 	protected $_log_files = array();
 
-	public function registerFile(string $identifier, string $directory, string $filename, bool $rotate = true, int $delete_after_days = 7): Log
+	/**
+	 * @param string $identifier
+	 * @param string $directory
+	 * @param bool $rotate
+	 * @param int|null $delete_after_days
+	 * @return Log
+	 * @throws \Exception
+	 */
+	public function registerFile(string $identifier, string $directory, bool $rotate = true, ?int $delete_after_days = 7): Log
 	{
 		if(empty($identifier))
 		{
 			throw new \Exception('Identifier cannot be empty', Codes::LOG_IDENTIFIER_MISSING);
 		}
 
+		if(file_exists($directory) && !is_dir($directory))
+		{
+			throw new \Exception('Directory supplied is not a directory.', Codes::LOG_INVALID_DIR);
+		}
+		elseif(!file_exists($directory))
+		{
+			mkdir($directory, 0775, true);
+		}
+
+		$filename = $identifier;
+
+		if($rotate)
+		{
+			$filename .= date('-Y-m-d');
+
+			if(!empty($delete_after_days))
+			{
+				$files = scandir($directory);
+
+				if(!empty($files))
+				{
+					foreach($files as $fileName)
+					{
+						if(PzPHP_Helper_IO::isValidDir($path.$fileName))
+						{
+							continue;
+						}
+
+						if(!preg_match(self::REGEX_LOG_PATTERN, $fileName, $matches))
+						{
+							continue;
+						}
+
+						if(isset($matches[1]))
+						{
+							$fileDate		= new DateTime($matches[1]);
+							$currentDate	= new DateTime(date('Y-m-d'));
+
+							$interval		= $fileDate->diff($currentDate);
+
+							if($interval->days >= PzPHP_Config::get('SETTING_DELETE_LOG_FILES_AFTER_DAYS'))
+							{
+								PzPHP_Helper_IO::removeFileFolderEnforce($path.$fileName);
+							}
+						}
+					}
+				}
+			}
+		}
+
 		$this->_log_files[$identifier] = array(
 			'directory' => $directory,
-			'filename' => $filename,
+			'filename' => $filename.'.log'
 		);
 
 		return $this;
