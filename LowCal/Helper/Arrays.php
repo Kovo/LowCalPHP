b41e3553d2796b64f9afc344d04b89fd17fd28e6
Kovo <kovo88@gmail.com>
2020-10-02 16:39:46 -0400

    features, bug fixes, progress

--- LowCal/Helper/Arrays.php
+++ LowCal/Helper/Arrays.php
@@ -236,4 +236,30 @@ class Arrays
 
 		return $return;
 	}
+
+	/**
+	 * @param $input
+	 * @param $offset
+	 * @param $length
+	 * @param array $replacement
+	 */
+	public static function array_splice_assoc(&$input, $offset, $length, $replacement = array())
+	{
+		$replacement = (array)$replacement;
+		$key_indices = array_flip(array_keys($input));
+
+		if(isset($input[$offset]) && is_string($offset))
+		{
+			$offset = $key_indices[$offset];
+		}
+
+		if(isset($input[$length]) && is_string($length))
+		{
+			$length = $key_indices[$length] - $offset;
+		}
+
+		$input = array_slice($input, 0, $offset, true)
+			+ $replacement
+			+ array_slice($input, $offset + $length, null, true);
+	}
 }
